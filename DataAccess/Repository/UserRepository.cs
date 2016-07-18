9bbd7f68be4991d81ef79c5bcbf150c85d94873a
Georgi Dobrishinov <dobrishinov@gmail.com>
2016-07-18 09:51:18 +0300

    Create DataAccess with Entity, Repository and Service

---
+++ DataAccess/Repository/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿namespace DataAccess.Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DataAccess.Entity;
+    using System.IO;
+
+    public class UserRepository : BaseRepository<UserEntity>
+    {
+        public UserRepository(string pathToFile)
+            : base(pathToFile)
+        {
+        }
+
+        protected override void WriteItemToStream(StreamWriter sw, UserEntity item)
+        {
+            sw.WriteLine(item.FirstName);
+            sw.WriteLine(item.LastName);
+            sw.WriteLine(item.Username);
+            sw.WriteLine(item.Password);
+            sw.WriteLine(item.AdminStatus);
+        }
+
+        protected override void ReadItemFromStream(StreamReader sr, UserEntity item)
+        {
+            item.FirstName = sr.ReadLine();
+            item.LastName = sr.ReadLine();
+            item.Username = sr.ReadLine();
+            item.Password = sr.ReadLine();
+            item.AdminStatus = Convert.ToBoolean(sr.ReadLine());
+        }
+
+        public UserEntity GetByUsernameAndPassword(string username, string password)
+        {
+            return GetAll().Find(u => u.Username == username && u.Password == password);
+        }
+    }
+}
